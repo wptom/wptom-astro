510663b7f33e52eeaf065908d97fd71986337c9e
Tomas Sramek <tom@wptom.com>
2024-08-10 20:56:52 +0200

    Styling

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -55,7 +55,7 @@ module.exports = {
         transparent: "transparent",
       },
       fontFamily: {
-        mono: ["Poppins", "monospace"],
+        mono: ["-apple-system", "BlinkMacSystemFont", "Segoe UI", "Roboto", "Oxygen-Sans", "Ubuntu", "Cantarell", "Helvetica Neue", "sans-serif"],
       },
 
       typography: {
